85c868bee029058eafc84f5e724a37f87fb0061d
zhengguang18282 <zhengguang.yang@advantech.com.cn>
2019-03-27 15:41:39 +0800

    1 add phy config notes 2 modify RXC SSC register address for last configuration error

--- board/ti/am335x_adv/mux_ubc440.c
+++ board/ti/am335x_adv/mux_ubc440.c
@@ -224,20 +224,24 @@ void enable_board_pin_mux(void)
 
 void config_phy_reg(const char *devname, unsigned char addr)
 {
-	miiphy_write(devname, addr, 0x1f, 0x0cc4);
+	/*Enable RXC SSC*/
+	miiphy_write(devname, addr, 0x1f, 0x0c44);
 	miiphy_write(devname, addr, 0x13, 0x5f00);
 	miiphy_write(devname, addr, 0x1f, 0x0000);
 	miiphy_write(devname, addr, 0x00, 0x9200);
 
+	/*CLK_OUT disable*/
 	miiphy_write(devname, addr, 0x1f, 0x0a43);
 	miiphy_write(devname, addr, 0x19, 0x0862);
 	miiphy_write(devname, addr, 0x1f, 0x0000);
 	miiphy_write(devname, addr, 0x00, 0x9200);
 
+	/*Enable TX_delay*/
 	miiphy_write(devname, addr, 0x1f, 0x0d08);
 	miiphy_write(devname, addr, 0x11, 0x0109);
 	miiphy_write(devname, addr, 0x1f, 0x0000);
 
+	/*Change PHY LED status*/
 	miiphy_write(devname, addr, 0x1f, 0x0d04);
 	miiphy_write(devname, addr, 0x10, 0x091b);
 	miiphy_write(devname, addr, 0x11, 0x0000);
